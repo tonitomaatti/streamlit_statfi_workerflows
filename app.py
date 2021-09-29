24d2c1ab945c200f6a47484bb6430fe6d9eb1b44
tonitomaatti <toni.turpeinen@helsinki.fi>
2021-09-29 18:49:15 +0300

    fetch tk data

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import numpy as np
-import pandas as pd
-import requests
 
-st.title('Työvoimavirrat')
+from fetch_data import fetch_siirtymat_tyomarkkinoilla
+
+
+st.title('Siirtymät työmarkkinoilla')
+
+df = fetch_siirtymat_tyomarkkinoilla()
+st.write(df)
---
+++ fetch_data.py
@@ -0,0 +1,39 @@
+import requests
+import pandas as pd
+from io import StringIO
+
+def fetch_siirtymat_tyomarkkinoilla():
+    body = {
+        "query": [
+            {"code": "Sukupuoli", "selection": {"filter": "item", "values": ["SSS"]}},
+            {
+                "code": "Tiedot",
+                "selection": {
+                    "filter": "item",
+                    "values": [
+                        "v1_to_1",
+                        "v1_to_2",
+                        "v1_to_4",
+                        "v2_to_1",
+                        "v2_to_2",
+                        "v2_to_4",
+                        "v4_to_1",
+                        "v4_to_2",
+                        "v4_to_4",
+                        "vJ_to_J",
+                    ],
+                },
+            },
+        ],
+        "response": {"format": "csv"},
+    }
+
+    r = requests.post('https://pxnet2.stat.fi:443/PXWeb/api/v1/fi/Kokeelliset_tilastot/tyvir/koeti_tyvir_pxt_12aj.px', json=body)
+    print(r)
+
+    df = pd.read_csv(StringIO(r.text), sep=",", index_col="Vuosineljännes")
+    df = df.drop(columns=["Sukupuoli"])
+
+    return df
+
+

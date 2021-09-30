2a7ba056e3276fd5c8963353eab9836dde5bcd04
tonitomaatti <toni.turpeinen@helsinki.fi>
2021-09-30 07:46:05 +0300

    putsausta

--- sankey.py
+++ sankey.py
@@ -32,21 +32,14 @@ def generate_sankey_data(df, quarters):
     red = "rgba(184,29,19, 0.4)"
     yellow = "rgba(239,183,0, 0.4)"
 
-    # darker2_green = 0.50
-    # darker1_green = "rgba(0,99,60, 0.4)"
-    # darkest_green =
-
     # Labelit, värit ja paikat nodeille
     labels = ["Työlliset"] + [None for i in range(1, rows_n)]
-    # node_colors = [green for i in range(0, rows_n)]
     node_colors = [greens[q_mapper[q]] for q in qs]
     node_x = list(np.linspace(0, 1, rows_n, endpoint=True))
     node_y = [0 for i in range(0, rows_n)]
 
     labels = labels + ["Työttömät"] + [None for i in range(1, rows_n)]
-    node_colors = node_colors + [
-        reds[q_mapper[q]] for q in qs
-    ]  # [red for i in range(0, rows_n)]
+    node_colors = node_colors + [reds[q_mapper[q]] for q in qs]
     node_x = node_x + list(np.linspace(0, 1, rows_n, endpoint=True))
     node_y = node_y + [0.5 for i in range(0, rows_n)]
 
@@ -54,14 +47,6 @@ def generate_sankey_data(df, quarters):
     node_colors = node_colors + [yellows[q_mapper[q]] for q in qs]
     node_x = node_x + list(np.linspace(0, 1, rows_n, endpoint=True))
     node_y = node_y + [0.9 for i in range(0, rows_n)]
-
-    # Värit nodeille
-    # node_color_map = {
-    #    "Työlliset": green,
-    #    "Työttömät": red,
-    #    "Työvoiman ulkopuolella": yellow,
-    # }
-    # node_colors = [node_color_map[node_type] for node_type in labels]
 
     source = []
     target = []
@@ -74,63 +59,54 @@ def generate_sankey_data(df, quarters):
         source.append(i)
         target.append(i + 1)
         value.append(df.iloc[i, 0])
-        # link_colors.append(green)
         link_colors.append(greens[q_mapper[qs[i]]])
 
         # Työllisestä työttömäksi
         source.append(i)
         target.append(tyottomat_offset + i + 1)
         value.append(df.iloc[i, 1])
-        # link_colors.append(green)
         link_colors.append(greens[q_mapper[qs[i]]])
 
         # Työttömästä työlliseksi
         source.append(tyottomat_offset + i)
         target.append(i + 1)
         value.append(df.iloc[i, 3])
-        # link_colors.append(red)
         link_colors.append(reds[q_mapper[qs[i]]])
 
         # Työttömänä pysyneet
         source.append(tyottomat_offset + i)
         target.append(tyottomat_offset + i + 1)
         value.append(df.iloc[i, 4])
-        # link_colors.append(red)
         link_colors.append(reds[q_mapper[qs[i]]])
 
         # Työllisestä työvoiman ulkopuolelle
         source.append(i)
         target.append(tyov_ulkop_offset + i + 1)
         value.append(df.iloc[i, 2])
-        # link_colors.append(green)
         link_colors.append(greens[q_mapper[qs[i]]])
 
         # Työttömästä työvoiman ulkopuolelle
         source.append(tyottomat_offset + i)
         target.append(tyov_ulkop_offset + i + 1)
         value.append(df.iloc[i, 5])
-        # link_colors.append(red)
         link_colors.append(reds[q_mapper[qs[i]]])
 
         # Työvoiman ulkopuolella pysyneet
         source.append(tyov_ulkop_offset + i)
         target.append(tyov_ulkop_offset + i + 1)
         value.append(df.iloc[i, 8])
-        # link_colors.append(yellow)
         link_colors.append(yellows[q_mapper[qs[i]]])
 
         # Työvoiman ulkopuolelta työlliseksi
         source.append(tyov_ulkop_offset + i)
         target.append(i + 1)
         value.append(df.iloc[i, 6])
-        # link_colors.append(yellow)
         link_colors.append(yellows[q_mapper[qs[i]]])
 
         # Työvoiman ulkopuolelta työttömäksi
         source.append(tyov_ulkop_offset + i)
         target.append(tyottomat_offset + i + 1)
         value.append(df.iloc[i, 7])
-        # link_colors.append(yellow)
         link_colors.append(yellows[q_mapper[qs[i]]])
 
     return labels, node_colors, source, target, value, link_colors, node_x, node_y
@@ -186,15 +162,14 @@ def generate_sankey_figure(df, quarters):
             "x": x_array[i],
             "y": 1,
             "showarrow": False,
-            "bgcolor": "white",
-            "font": {"color": "red", "size": 15},
+            "bgcolor": None,
+            "font": {"color": "red", "size": 10},
             "borderpad": 5,
         }
         for i in range(0, len(quarters))
     ]
 
-    fig.update_layout(
-        margin=dict(l=0, r=0, t=0, b=0),
-    )  # annotations=annotations
+    fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
+    # annotations=annotations
 
     return fig

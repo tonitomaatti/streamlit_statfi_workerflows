cfcaea91c433c5697fafc48872877ffe4d66139c
tonitomaatti <toni.turpeinen@helsinki.fi>
2021-09-30 05:36:54 +0300

    improved sankey

--- app.py
+++ app.py
@@ -15,20 +15,65 @@ quarters = list(df.index.values)
 
 # Aloitetaan df yksi eteenpäin: Siirtymät työmarkkinoilla edellisestä vuosineljänneksestä,
 
-with st.container():
+start, end = st.select_slider(
+    label="",
+    options=[i for i in range(0, len(quarters))],  # quarters,
+    value=(0, 9),
+    format_func=lambda x: quarters[x],
+)
 
-    start, end = st.select_slider(
-        label="",
-        options=[i for i in range(0, len(quarters))],  # quarters,
-        value=(0, 9),
-        format_func=lambda x: quarters[x],
-    )
+col1, col2 = st.columns([5, 1])
 
-    df_start = start if start == end else start + 1
+df_start = start if start == end else start + 1
 
-    df_to_display = df.iloc[df_start : end + 1]
-    fig = generate_sankey_figure(df_to_display)
-    st.plotly_chart(fig, use_container_width=True)
-    st.write(df_to_display)
+df_to_display = df.iloc[df_start : end + 1]
+fig = generate_sankey_figure(
+    df_to_display, list(df.iloc[df_start - 1 : end + 1].index.values)
+)
 
-# st.write(df_to_display)
+# print(fig.__dict__)
+# st.write(range_slider)
+
+col1.plotly_chart(fig, use_container_width=True)
+
+## Metrics
+# Calculate start and end
+tyolliset_start = (
+    df_to_display.iloc[0, 0] + df_to_display.iloc[0, 1] + df_to_display.iloc[0, 2]
+)
+tyottomat_start = (
+    df_to_display.iloc[0, 3] + df_to_display.iloc[0, 4] + df_to_display.iloc[0, 5]
+)
+tyov_ulkop_start = (
+    df_to_display.iloc[0, 6] + df_to_display.iloc[0, 7] + df_to_display.iloc[0, 8]
+)
+
+tyolliset_end = (
+    df_to_display.iloc[-1, 0] + df_to_display.iloc[-1, 3] + df_to_display.iloc[-1, 6]
+)
+tyottomat_end = (
+    df_to_display.iloc[-1, 1] + df_to_display.iloc[-1, 4] + df_to_display.iloc[-1, 7]
+)
+tyov_ulkop_end = (
+    df_to_display.iloc[-1, 2] + df_to_display.iloc[-1, 5] + df_to_display.iloc[-1, 8]
+)
+
+tyolliset_change = tyolliset_end - tyolliset_start
+tyottomat_change = tyottomat_end - tyottomat_start
+tyov_ulkop_change = tyov_ulkop_end - tyov_ulkop_start
+
+col2.metric(label="Työlliset", value=str(tyolliset_end), delta=str(tyolliset_change))
+col2.metric(
+    label="Työvoiman ulkopuolella",
+    value=str(tyov_ulkop_end),
+    delta=str(tyov_ulkop_change),
+    delta_color="off",
+)
+col2.metric(
+    label="Työttömät",
+    value=str(tyottomat_end),
+    delta=str(tyottomat_change),
+    delta_color="inverse",
+)
+
+st.write(df_to_display)
--- sankey.py
+++ sankey.py
@@ -1,5 +1,6 @@
 import plotly.graph_objects as go
 import streamlit as st
+import numpy as np
 
 
 @st.cache(suppress_st_warning=True)
@@ -11,23 +12,34 @@ def generate_sankey_data(df):
     tyottomat_offset = rows_n
     tyov_ulkop_offset = tyottomat_offset + rows_n
 
-    # Labelit nodeille
-    labels = ["Työlliset" for i in range(0, rows_n)]
-    labels = labels + ["Työttömät" for i in range(0, rows_n)]
-    labels = labels + ["Työvoiman ulkopuolella" for i in range(0, rows_n)]
-
     # Color palette:
     green = "rgba(0,132,80, 0.4)"
     red = "rgba(184,29,19, 0.4)"
     yellow = "rgba(239,183,0, 0.4)"
 
+    # Labelit, värit ja paikat nodeille
+    labels = ["Työlliset"] + [None for i in range(1, rows_n)]
+    node_colors = [green for i in range(0, rows_n)]
+    node_x = list(np.linspace(0, 1, rows_n, endpoint=True))
+    node_y = [0 for i in range(0, rows_n)]
+
+    labels = labels + ["Työttömät"] + [None for i in range(1, rows_n)]
+    node_colors = node_colors + [red for i in range(0, rows_n)]
+    node_x = node_x + list(np.linspace(0, 1, rows_n, endpoint=True))
+    node_y = node_y + [0.5 for i in range(0, rows_n)]
+
+    labels = labels + ["Työvoiman ulkopuolella"] + [None for i in range(1, rows_n)]
+    node_colors = node_colors + [yellow for i in range(0, rows_n)]
+    node_x = node_x + list(np.linspace(0, 1, rows_n, endpoint=True))
+    node_y = node_y + [0.9 for i in range(0, rows_n)]
+
     # Värit nodeille
-    node_color_map = {
-        "Työlliset": green,
-        "Työttömät": red,
-        "Työvoiman ulkopuolella": yellow,
-    }
-    node_colors = [node_color_map[node_type] for node_type in labels]
+    # node_color_map = {
+    #    "Työlliset": green,
+    #    "Työttömät": red,
+    #    "Työvoiman ulkopuolella": yellow,
+    # }
+    # node_colors = [node_color_map[node_type] for node_type in labels]
 
     source = []
     target = []
@@ -90,32 +102,66 @@ def generate_sankey_data(df):
         value.append(df.iloc[i, 7])
         link_colors.append(yellow)
 
-    return labels, node_colors, source, target, value, link_colors
+    return labels, node_colors, source, target, value, link_colors, node_x, node_y
 
 
-@st.cache(suppress_st_warning=True)
-def generate_sankey_figure(df):
-    labels, node_colors, source, target, value, link_colors = generate_sankey_data(df)
+@st.cache(suppress_st_warning=True, allow_output_mutation=True)
+def generate_sankey_figure(df, quarters):
+    (
+        labels,
+        node_colors,
+        source,
+        target,
+        value,
+        link_colors,
+        node_x,
+        node_y,
+    ) = generate_sankey_data(df)
 
     fig = go.Figure(
         data=[
             go.Sankey(
-                node={"pad": 10, "label": labels, "color": node_colors},
+                arrangement="snap",
+                node={
+                    "pad": 5,
+                    "thickness": 15,
+                    "label": labels,
+                    "color": node_colors,
+                    "hovertemplate": "Kokonaismäärä",
+                },
                 link={
                     "source": source,
                     "target": target,
                     "value": value,
                     "color": link_colors,
+                    "hovertemplate": "Siirtymä",
                 },
-            )
-        ]
+            ),
+        ],
     )
 
-    # fig.update_layout(
-    #    title_text="Työvoiman liikkeet kvartaalista x kvartaaliin y", font_size=10
-    # )
+    x_array = np.linspace(0, 1, len(quarters), endpoint=True)
+    offset = 0.001
+    x_array = x_array + offset
+
+    annotations = [
+        {
+            "text": quarters[i],
+            "textangle": 90,
+            "xref": "x domain",
+            "yref": "y domain",
+            "x": x_array[i],
+            "y": 1,
+            "showarrow": False,
+            "bgcolor": "white",
+            "font": {"color": "red", "size": 15},
+            "borderpad": 5,
+        }
+        for i in range(0, len(quarters))
+    ]
+
     fig.update_layout(
         margin=dict(l=0, r=0, t=0, b=0),
-    )
+    )  # annotations=annotations
 
     return fig
